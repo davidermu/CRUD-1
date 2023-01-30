05466e4925efbdf66efe73209d00a638b8f5e017
Charlie Mendoza <56177445+Gutsnt@users.noreply.github.com>
2023-01-30 10:55:04 -0400

    actualizado

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import dbConnect from '../../../utils/mongoose'
+import dbConnect from 'utils/mongoose'
+import Task from 'models/Tasks'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 dbConnect();
 
-import type { NextApiRequest, NextApiResponse } from 'next'
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function  handler(req: NextApiRequest,res: NextApiResponse){
+  const task = await Task.find()
+  console.log(task)
+
   res.status(200).json("tasks")
 }
--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -1,11 +1,17 @@
 import {connect,connection} from 'mongoose';
+import { CLIENT_RENEG_LIMIT } from 'tls';
 
+const conn = {
+    isConnected: false
+}
 const dbConnect = async ()=>{
+    if (conn.isConnected) return;
 
-    const db = await connect(process.env.MONGOB_URL)
+    const db = await connect(process.env.MONGOB_URL as string)
 
-    console.log(db.connections[0].readyState);
-    
+    conn.isConnected  =  db.connections[0].readyState 
+
+    console.log(db.connection.db.databaseName);
 }
 
 connection.on("connected",()=>console.log("connected to db"));
